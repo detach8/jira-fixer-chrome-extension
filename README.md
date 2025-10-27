9f3062c6cd4c0639320e6e523273bb09b41c1f7b
Justin Lee <tzlee@tzlee.com>
2025-10-27 12:13:46 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Jira Fixer
+
+This Chrome Extension fixes some of Atlassian's bad UI/UX decisions, such as
+renaming 'Projects' to 'Spaces' in Jira. This renames it back.
+
+## Running this extension
+
+* Clone this repository.
+* Load this directory in Chrome as an unpacked extension.
---
+++ jira-fixer.js
@@ -0,0 +1,11 @@
+const spans = document.querySelectorAll("span");
+spans.forEach((element) => {
+  renameSpacesAsProjects(element);
+});
+
+function renameSpacesAsProjects(element) {
+  const text = element.innerHTML;
+  if (/spaces/i.test(text)) {
+    element.innerHTML = text.replace(/spaces/gi, "Projects");
+  }
+}
